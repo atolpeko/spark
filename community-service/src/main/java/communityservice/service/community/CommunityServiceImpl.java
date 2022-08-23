eba983100ccaced6d849fcda776defacafceaeea
Alexander Tolpeko <alextolpeko@gmail.com>
2022-08-23 16:26:05 +0200

    Comunity service: created community service class

---
+++ community-service/src/main/java/communityservice/service/community/CommunityServiceImpl.java
@@ -0,0 +1,191 @@
+/*
+ * Copyright 2002-2021 Alexander Tolpeko.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package communityservice.service.community;
+
+import communityservice.data.CommunityRepository;
+import communityservice.service.exception.IllegalModificationException;
+import communityservice.service.exception.RemoteResourceException;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Supplier;
+
+@Service
+@Transactional
+public class CommunityServiceImpl implements CommunityService {
+    private static final Logger logger = LogManager.getLogger(CommunityServiceImpl.class);
+
+    private final CommunityRepository repository;
+    private final Validator validator;
+    private final CircuitBreaker circuitBreaker;
+
+    @Autowired
+    public CommunityServiceImpl(CommunityRepository repository,
+                                Validator validator,
+                                CircuitBreaker circuitBreaker) {
+        this.repository = repository;
+        this.validator = validator;
+        this.circuitBreaker = circuitBreaker;
+    }
+
+    @Override
+    public List<Community> findAll() {
+        try {
+            Supplier<List<Community>> findAll = repository::findAll;
+            List<Community> communities = circuitBreaker.decorateSupplier(findAll).get();
+            Collections.sort(communities);
+            return communities;
+        } catch (Exception e) {
+            throw new RemoteResourceException("Community database unavailable", e);
+        }
+    }
+
+    @Override
+    public Optional<Community> findById(long id) {
+        try {
+            Supplier<Optional<Community>> findById = () -> repository.findById(id);
+            return circuitBreaker.decorateSupplier(findById).get();
+        } catch (Exception e) {
+            throw new RemoteResourceException("Community database unavailable", e);
+        }
+    }
+
+    @Override
+    public Optional<Community> findByName(String name) {
+        try {
+            Supplier<Optional<Community>> findByName = () -> repository.findByName(name);
+            return circuitBreaker.decorateSupplier(findByName).get();
+        } catch (Exception e) {
+            throw new RemoteResourceException("Community database unavailable", e);
+        }
+    }
+
+    @Override
+    public Community save(Community community) {
+        try {
+            validate(community);
+            Community communityToSave = prepareSaveData(community);
+            Community saved = persistCommunity(communityToSave);
+            logger.info("Community " + saved.getId() + " saved");
+            return saved;
+        } catch (IllegalModificationException | RemoteResourceException e) {
+            throw e;
+        } catch (DataIntegrityViolationException e) {
+            String msg = "Such a community already exists: " + community.getId();
+            throw new IllegalModificationException(msg, e);
+        } catch (Exception e) {
+            throw new RemoteResourceException("Community database unavailable", e);
+        }
+    }
+
+    private void validate(Community community) {
+        Set<ConstraintViolation<Community>> violations = validator.validate(community);
+        if (!violations.isEmpty()) {
+            StringBuilder builder = new StringBuilder();
+            for (ConstraintViolation<Community> violation : violations) {
+                builder.append(violation.getMessage()).append(", ");
+            }
+
+            builder.delete(builder.length() - 2, builder.length() - 1);
+            String msg = builder.toString().toLowerCase(Locale.ROOT);
+            throw new IllegalModificationException(msg);
+        }
+    }
+
+    private Community prepareSaveData(Community community) {
+        Community communityToSave = new Community(community);
+        communityToSave.setId(null);
+
+        return communityToSave;
+    }
+
+    private Community persistCommunity(Community community) {
+        Supplier<Community> save = () -> {
+            Community saved = repository.save(community);
+            repository.flush();
+            return saved;
+        };
+
+        return circuitBreaker.decorateSupplier(save).get();
+    }
+
+    @Override
+    public Community update(Community community) {
+        try {
+            long id = community.getId();
+            Community communityToUpdate = findById(id)
+                    .orElseThrow(() -> new IllegalModificationException("No community with ID " + id));
+            communityToUpdate = prepareUpdateData(communityToUpdate, community);
+            validate(communityToUpdate);
+
+            Community updated = persistCommunity(communityToUpdate);
+            logger.info("Community " + updated.getId() + " updated");
+            return updated;
+        } catch (IllegalModificationException | RemoteResourceException e) {
+            throw e;
+        } catch (DataIntegrityViolationException e) {
+            String msg = "Such a community already exists: " + community.getId();
+            throw new IllegalModificationException(msg, e);
+        } catch (Exception e) {
+            throw new RemoteResourceException("Community database unavailable", e);
+        }
+    }
+
+    private Community prepareUpdateData(Community savedCommunity, Community updateData) {
+        return Community.builder(savedCommunity)
+                .copyNonNullFields(updateData)
+                .build();
+    }
+
+    @Override
+    public void deleteById(long id) {
+        try {
+            deleteCommunity(id);
+            logger.info("Community " + id + " deleted");
+        } catch (EmptyResultDataAccessException e) {
+            throw new IllegalModificationException("No community with ID " + id, e);
+        } catch (Exception e) {
+            throw new RemoteResourceException("Community database unavailable", e);
+        }
+    }
+
+    private void deleteCommunity(long id) {
+        Runnable delete = () -> {
+            repository.deleteById(id);
+            repository.flush();
+        };
+
+        circuitBreaker.decorateRunnable(delete).run();
+    }
+}
---
+++ community-service/src/test/java/communityservice/service/community/CommunityServiceImplTest.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright 2002-2021 Alexander Tolpeko.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package communityservice.service.community;
+
+import communityservice.data.CommunityRepository;
+import communityservice.service.exception.IllegalModificationException;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import org.mockito.Mockito;
+
+import javax.validation.Validator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@Tag("category.UnitTest")
+public class CommunityServiceImplTest {
+    private static CommunityRepository communityRepository;
+    private static Validator validator;
+    private static CircuitBreaker circuitBreaker;
+
+    private static Community community;
+    private static Community updatedCommunity;
+
+    private CommunityServiceImpl communityService;
+
+    @BeforeAll
+    public static void setUpMocks() {
+        communityRepository = mock(CommunityRepository.class);
+        validator = mock(Validator.class);
+
+        circuitBreaker = mock(CircuitBreaker.class);
+        when(circuitBreaker.decorateSupplier(any())).then(returnsFirstArg());
+        when(circuitBreaker.decorateRunnable(any())).then(returnsFirstArg());
+    }
+
+    @BeforeAll
+    public static void createCommunity() {
+        community = Community.builder()
+                .withId(1L)
+                .withName("name")
+                .withDescription("desc")
+                .build();
+    }
+
+    @BeforeAll
+    public static void createUpdatedCommunity() {
+        updatedCommunity = Community.builder()
+                .withId(1L)
+                .withName("name2")
+                .withDescription("desc2")
+                .build();
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        Mockito.reset(communityRepository, validator);
+        communityService = new CommunityServiceImpl(communityRepository, validator, circuitBreaker);
+    }
+
+    @Test
+    public void shouldReturnCommunityByIdWhenContainsIt() {
+        when(communityRepository.findById(community.getId())).thenReturn(Optional.of(community));
+
+        Community saved = communityService.findById(community.getId()).orElseThrow();
+        assertThat(saved, is(equalTo(community)));
+    }
+
+    @Test
+    public void shouldReturnCommunityByNameWhenContainsIt() {
+        when(communityRepository.findByName(community.getName())).thenReturn(Optional.of(community));
+
+        Community saved = communityService.findByName(community.getName()).orElseThrow();
+        assertThat(saved, is(equalTo(community)));
+    }
+
+    @Test
+    public void shouldReturnListOfCommunitiesWhenContainsMultipleCommunities() {
+        List<Community> communities = new ArrayList<>(List.of(community, community, community));
+        when(communityRepository.findAll()).thenReturn(communities);
+
+        List<Community> saved = communityService.findAll();
+        assertThat(saved, is(equalTo(communities)));
+    }
+
+    @Test
+    public void shouldSaveCommunityWhenCommunityIsValid() {
+        when(communityRepository.save(any(Community.class))).thenReturn(community);
+        when(validator.validate(any(Community.class))).thenReturn(Collections.emptySet());
+
+        Community saved = communityService.save(community);
+        assertThat(saved, equalTo(community));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenCommunityIsInvalid() {
+        when(validator.validate(any(Community.class))).thenThrow(IllegalModificationException.class);
+        assertThrows(IllegalModificationException.class, () -> communityService.save(new Community()));
+    }
+
+    @Test
+    public void shouldUpdateCommunityWhenCommunityIsValid() {
+        when(communityRepository.findById(1L)).thenReturn(Optional.of(community));
+        when(communityRepository.save(any(Community.class))).thenReturn(updatedCommunity);
+        when(validator.validate(any(Community.class))).thenReturn(Collections.emptySet());
+
+        Community updated = communityService.update(updatedCommunity);
+        assertThat(updated, equalTo(updatedCommunity));
+    }
+
+    @Test
+    public void shouldNotContainCommunityWhenDeletesThisCommunity() {
+        when(communityRepository.findById(any(Long.class))).thenReturn(Optional.of(community));
+        doAnswer(invocation -> when(communityRepository.findById(1L)).thenReturn(Optional.empty()))
+                .when(communityRepository).deleteById(1L);
+
+        communityService.deleteById(1L);
+
+        Optional<Community> deleted = communityService.findById(1L);
+        assertThat(deleted, is(Optional.empty()));
+    }
+}
