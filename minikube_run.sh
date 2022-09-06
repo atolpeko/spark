fc6ebc7757d82761f2256786d37aa7a6108fff07
Alexander Tolpeko <alextolpeko@gmail.com>
2022-09-06 12:54:35 +0200

    Created run shell script

---
+++ minikube_run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Author : Alexander Tolpeko
+
+echo "Spinning up kubernetes cluster"
+
+kubectl create -f deployment/mysql/storage.yaml 
+kubectl create -f deployment/mysql/secret.yaml 
+kubectl create -f deployment/mysql/mysql-deployment.yaml 
+
+kubectl create -f deployment/auth-service/secret.yaml
+envsubst < deployment/auth-service/auth-service-deployment.yaml | kubectl apply -f -
+
+kubectl create -f deployment/user-service/secret.yaml
+envsubst < deployment/user-service/user-service-deployment.yaml | kubectl apply -f -
+
+kubectl create -f deployment/community-service/secret.yaml
+envsubst < deployment/community-service/community-service-deployment.yaml | kubectl apply -f -
+
+echo "System started"
