7ad090c761e0bf5f02b60028fefa85f984e1ee0c
Alexander Tolpeko <alextolpeko@gmail.com>
2022-09-06 20:42:38 +0200

    Ingress paths prefixed with /api

--- minikube_run.sh
+++ minikube_run.sh
@@ -7,13 +7,15 @@ kubectl create -f deployment/mysql/storage.yaml
 kubectl create -f deployment/mysql/secret.yaml 
 kubectl create -f deployment/mysql/mysql-deployment.yaml 
 
-kubectl create -f deployment/auth-service/secret.yaml
-envsubst < deployment/auth-service/auth-service-deployment.yaml | kubectl apply -f -
+kubectl create -f deployment/ingress.yaml 
 
 kubectl create -f deployment/user-service/secret.yaml
 envsubst < deployment/user-service/user-service-deployment.yaml | kubectl apply -f -
 
 kubectl create -f deployment/community-service/secret.yaml
 envsubst < deployment/community-service/community-service-deployment.yaml | kubectl apply -f -
+
+kubectl create -f deployment/auth-service/secret.yaml
+envsubst < deployment/auth-service/auth-service-deployment.yaml | kubectl apply -f -
 
 echo "System started"
